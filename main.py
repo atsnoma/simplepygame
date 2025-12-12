b143570b092c04f5eb175d9cda97fa8a1c7d05db
atsnoma <misterleafjuice@gmail.com>
2025-12-12 12:07:51 -0800

    Initial commit Simple movement of a square on a screen

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import pygame as pg
+
+pg.init()
+
+width, height = 480, 320
+xbox, ybox = 20, 20
+startx, starty = width/2, height/2
+screen = pg.display.set_mode((width, height))
+clock = pg.time.Clock()
+main_ship = pg.Rect(startx, starty, xbox, ybox)
+running = True
+while running:
+ 
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_LEFT]:
+        main_ship.x -= 3
+        if main_ship.x < 0 : main_ship.x = 0
+
+    if keys[pg.K_RIGHT]:
+        main_ship.x += 3
+        if main_ship.x > width-xbox : main_ship.x = width-xbox
+    
+    if keys[pg.K_UP]:
+        main_ship.y -= 3
+        if main_ship.y < 0 : main_ship.y = 0
+    
+    if keys[pg.K_DOWN]:
+        main_ship.y += 3
+        if main_ship.y > height-ybox: main_ship.y = height-ybox
+
+    screen.fill((20, 20, 30))
+    pg.draw.rect(screen, (80, 80, 80), main_ship)
+
+    pg.display.flip()
+
+    clock.tick(60)
+
+# End of Game
+pg.quit() 
+sys.exit()
